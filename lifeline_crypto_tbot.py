c400bddc792a2510faf3c0a477a60d1c4954a796
Restyled.io <commits@restyled.io>
2021-05-07 23:50:50 +0000

    Restyled by black

--- lifeline_crypto_tbot.py
+++ lifeline_crypto_tbot.py
@@ -79,8 +79,7 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", start))
-    dp.add_handler(MessageHandler(
-        Filters.status_update.new_chat_members, greet))
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, greet))
 
     # log all errors
     dp.add_error_handler(error)
