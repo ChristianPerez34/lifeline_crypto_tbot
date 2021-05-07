07ec7367f3a68bceead7eb8655abae9c1c01eea4
Restyled.io <commits@restyled.io>
2021-05-07 19:04:49 +0000

    Restyled by yapf

--- lifeline_crypto_tbot.py
+++ lifeline_crypto_tbot.py
@@ -9,16 +9,15 @@ from telegram.ext import Updater
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
