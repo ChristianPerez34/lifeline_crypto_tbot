48bb2b8dc7813d5dc8f9ba3be1c2db9881beaf8d
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2021-05-09 21:06:44 -0400

    Added environment variables for etherscan api and bot environment. VSCode configuration to launch bot in debug mode. Gas prices in GWEI

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from etherscan import Etherscan
 
 from coinmarketcapapi import CoinMarketCapAPI
 from lru import LRU
@@ -7,17 +8,18 @@ from pycoingecko import CoinGeckoAPI
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
-cmc = CoinMarketCapAPI(os.getenv("COIN_MARKET_CAP_API"))
-cg = CoinGeckoAPI()
 crypto_cache = LRU(5)
+cmc = CoinMarketCapAPI(os.getenv("COIN_MARKET_CAP_API_KEY"))
+cg = CoinGeckoAPI()
+eth = Etherscan(os.getenv("ETHERSCAN_API_KEY"))
