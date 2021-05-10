825a55caab60dab08beb8e422cee318bb7434fed
Restyled.io <commits@restyled.io>
2021-05-10 01:08:57 +0000

    Restyled by reorder-python-imports

--- handler/crypto.py
+++ handler/crypto.py
@@ -2,7 +2,10 @@ import emoji
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.update import Update
 
-from . import cg, crypto_cache, eth, logger
+from . import cg
+from . import crypto_cache
+from . import eth
+from . import logger
 
 
 def coingecko_coin_lookup(ids: str) -> dict:
