cb4d03b29afcf41e84e6ae77675b8d9b845069ef
Restyled.io <commits@restyled.io>
2021-05-14 01:23:53 +0000

    Restyled by black

--- app/lifeline_crypto_tbot.py
+++ app/lifeline_crypto_tbot.py
@@ -36,8 +36,7 @@ def setup_handlers(dp: Dispatcher):
     dp.register_message_handler(send_coin_address, commands=["coin_address"])
     dp.register_message_handler(send_trending, commands=["trending"])
     dp.register_message_handler(send_price_alert, commands=["alert"])
-    dp.register_message_handler(
-        send_latest_listings, commands=["latest_listings"])
+    dp.register_message_handler(send_latest_listings, commands=["latest_listings"])
     dp.register_message_handler(send_greeting)
     dp.register_errors_handler(send_error),
 
