627614856d77c4ce4e0ba1ec5cc7203bf5ad0a13
Restyled.io <commits@restyled.io>
2021-05-18 17:01:15 +0000

    Restyled by isort

--- bot/kucoin_bot.py
+++ bot/kucoin_bot.py
@@ -1,15 +1,11 @@
 import asyncio
 
-from aiogram.utils.markdown import bold
-from aiogram.utils.markdown import text
+from aiogram.utils.markdown import bold, text
 from kucoin_futures.client import WsToken
 from kucoin_futures.ws_client import KucoinFuturesWsClient
 
-from bot import KUCOIN_API_KEY
-from bot import KUCOIN_API_PASSPHRASE
-from bot import KUCOIN_API_SECRET
-from bot import TELEGRAM_CHAT_ID
-from bot import active_orders
+from bot import (KUCOIN_API_KEY, KUCOIN_API_PASSPHRASE, KUCOIN_API_SECRET,
+                 TELEGRAM_CHAT_ID, active_orders)
 from handler import logger
 from handler.base import send_message
 
