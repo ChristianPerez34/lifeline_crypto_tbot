bf7505b9830e9cabbea30d91d83b5ff56a661673
Christian Perez Villanueva <perez.villanueva.christian34@gmail.com>
2021-05-18 12:56:42 -0400

    Bot alerts on position updates for take profit and stop loss

--- bot/kucoin_bot.py
+++ bot/kucoin_bot.py
@@ -35,13 +35,16 @@ async def kucoin_bot():
 
                     if side == order["side"]:
                         message = text(
-                            (f"Futures Contract ⏳\n\n"
-                             f"Coin: {bold(symbol)}\n"
-                             f"LONG/SHORT: {bold(side)}\n"
-                             f"Entry: {entry}\n"
-                             f"Leverage: {bold('10')}-{bold('20x')}\n"
-                             f"Take Profit: {bold('At Your Discretion')}\n"
-                             f"Stop Loss: {bold('At Your Discretion')}\n"))
+                            (
+                                f"Futures Contract ⏳\n\n"
+                                f"Coin: {bold(symbol)}\n"
+                                f"LONG/SHORT: {bold(side)}\n"
+                                f"Entry: {entry}\n"
+                                f"Leverage: {bold('10')}-{bold('20x')}\n"
+                                f"Take Profit: {bold('At Your Discretion')}\n"
+                                f"Stop Loss: {bold('At Your Discretion')}\n"
+                            )
+                        )
                     else:
                         active_orders.pop(symbol, None)
                 await send_message(channel_id=TELEGRAM_CHAT_ID, text=message)
@@ -53,22 +56,49 @@ async def kucoin_bot():
                 entry = data["matchPrice"]
                 if symbol in active_orders:
                     active_orders[symbol][
-                        "entry"] = f"{active_orders[symbol]['entry']}-{entry}"
+                        "entry"
+                    ] = f"{active_orders[symbol]['entry']}-{entry}"
                 else:
                     active_orders[symbol] = {
                         "entry": entry,
                         "side": "SHORT" if data["side"] == "sell" else "LONG",
                     }
+        elif msg['topic'] == '/contractMarket/advancedOrders':
+            data = msg['data']
+            if data['type'] != 'cancel':
+                symbol = data['symbol'][:-1]
+                order = active_orders[symbol]
+                stop_price = data['stopPrice']
+                if data['stop'] == 'up' and order['take_profit'] != stop_price:
+                    order['take_profit'] = stop_price
+                else:
+                    order['stop_loss'] = stop_price
+                message = text(
+                    (
+                        f"Futures Contract ⏳\n\nPosition Update ❗️❗️❗️\n\n"
+                        f"Coin: {bold(symbol)}\n"
+                        f"LONG/SHORT: {order['side']}\n"
+                        f"Entry: {order['entry']}\n"
+                        f"Leverage: 10-20x\n"
+                        f"Take Profit: {order['take_profit']}\n"
+                        f"Stop Loss: {order['stop_loss']}\n"
+                    )
+                )
+                await send_message(channel_id=TELEGRAM_CHAT_ID, text=message)
 
     # is private
-    client = WsToken(key=KUCOIN_API_KEY, secret=KUCOIN_API_SECRET, passphrase=KUCOIN_API_PASSPHRASE, is_sandbox=False,
-                     url='')
+    client = WsToken(
+        key=KUCOIN_API_KEY,
+        secret=KUCOIN_API_SECRET,
+        passphrase=KUCOIN_API_PASSPHRASE,
+        is_sandbox=False,
+        url="",
+    )
     loop = asyncio.get_event_loop()
-    ws_client = await KucoinFuturesWsClient.create(loop,
-                                                   client,
-                                                   deal_msg,
-                                                   private=True)
+    ws_client = await KucoinFuturesWsClient.create(loop, client, deal_msg, private=True)
+
     await ws_client.subscribe("/contractMarket/tradeOrders")
+    await ws_client.subscribe("/contractMarket/advancedOrders")
 
     while True:
         await asyncio.sleep(20)
