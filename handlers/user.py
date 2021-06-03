75bac90071e1eb181b29e2e83cf04844adf63916
Restyled.io <commits@restyled.io>
2021-06-03 15:36:38 +0000

    Restyled by isort

--- handlers/user.py
+++ handlers/user.py
@@ -2,8 +2,7 @@ from aiogram.types import Message
 from cryptography.fernet import Fernet
 
 from app import bot
-from config import FERNET_KEY
-from config import RegisterTypes
+from config import FERNET_KEY, RegisterTypes
 from handlers import logger
 from handlers.base import send_message
 from models import TelegramGroupMember
