d2d242b0b6b29fd0233852b7c8699de8334d6568
Christian Perez Villanueva <perez.villanueva.christian34@gmail.com>
2021-06-03 22:22:11 -0400

    Added exception handling to kucoin bot to prevent bot from continuing operations

--- bot/kucoin_bot.py
+++ bot/kucoin_bot.py
@@ -16,84 +16,87 @@ from handlers.base import send_message
 
 async def kucoin_bot():
     async def deal_msg(msg):
-        if msg["topic"] == "/contractMarket/tradeOrders":
-            data = msg["data"]
-            logger.info(data)
-            inline = False
+        try:
+            if msg["topic"] == "/contractMarket/tradeOrders":
+                data = msg["data"]
+                logger.info(data)
+                inline = False
 
-            if data["type"] == "filled":
-                symbol = data["symbol"][:-1]
-                symbol = symbol.replace("XBTUSDT", "BTCUSDT")
-                pnl = active_orders[symbol].get("pnl", "")
-                message = f"Futures Contract ⌛️\n\nCoin: {bold(symbol)}\nClosed Position\nPNL: {pnl}"
+                if data["type"] == "filled":
+                    symbol = data["symbol"][:-1]
+                    symbol = symbol.replace("XBTUSDT", "BTCUSDT")
+                    pnl = active_orders[symbol].get("pnl", "")
+                    message = f"Futures Contract ⌛️\n\nCoin: {bold(symbol)}\nClosed Position\nPNL: {pnl}"
 
-                if symbol in active_orders:
-                    order = active_orders[symbol]
+                    if symbol in active_orders:
+                        order = active_orders[symbol]
 
-                    entry = order["entry"]
-                    side = "SHORT" if data["side"] == "sell" else "LONG"
+                        entry = order["entry"]
+                        side = "SHORT" if data["side"] == "sell" else "LONG"
 
-                    if side == order["side"]:
-                        message = text(
-                            (f"Futures Contract ⏳\n\n"
-                             f"Coin: {bold(symbol)}\n"
-                             f"LONG/SHORT: {bold(side)}\n"
-                             f"Entry: {entry}\n"
-                             f"Leverage: {bold('10')}-{bold('20x')}\n"
-                             f"Take Profit: {bold('At Your Discretion')}\n"
-                             f"Stop Loss: {bold('At Your Discretion')}\n"))
-                        inline = True
-                        data = f"{symbol};{side};"
+                        if side == order["side"]:
+                            message = text(
+                                (f"Futures Contract ⏳\n\n"
+                                 f"Coin: {bold(symbol)}\n"
+                                 f"LONG/SHORT: {bold(side)}\n"
+                                 f"Entry: {entry}\n"
+                                 f"Leverage: {bold('10')}-{bold('20x')}\n"
+                                 f"Take Profit: {bold('At Your Discretion')}\n"
+                                 f"Stop Loss: {bold('At Your Discretion')}\n"))
+                            inline = True
+                            data = f"{symbol};{side};"
+                        else:
+                            data = ""
+                            active_orders.pop(symbol, None)
+                    await send_message(channel_id=TELEGRAM_CHAT_ID,
+                                       text=message,
+                                       inline=inline,
+                                       data=data)
+                elif data["type"] == "match":
+                    symbol = data["symbol"][:-1]
+
+                    # Webhook displays BTCUSDT symbol as XBTUSDT, so must be replaced with appropriate symbol
+                    symbol = symbol.replace("XBTUSDT", "BTCUSDT")
+                    entry = data["matchPrice"]
+                    if symbol in active_orders:
+                        active_orders[symbol][
+                            "entry"] = f"{active_orders[symbol]['entry']}-{entry}"
                     else:
-                        data = ""
-                        active_orders.pop(symbol, None)
-                await send_message(channel_id=TELEGRAM_CHAT_ID,
-                                   text=message,
-                                   inline=inline,
-                                   data=data)
-            elif data["type"] == "match":
-                symbol = data["symbol"][:-1]
-
-                # Webhook displays BTCUSDT symbol as XBTUSDT, so must be replaced with appropriate symbol
+                        active_orders[symbol] = {
+                            "entry": entry,
+                            "side": "SHORT" if data["side"] == "sell" else "LONG",
+                            "take_profit": "",
+                            "stop_loss": "",
+                            "pnl": "",
+                        }
+            elif msg["topic"] == "/contractMarket/advancedOrders":
+                data = msg["data"]
+                if data["type"] != "cancel":
+                    symbol = data["symbol"][:-1]
+                    symbol = symbol.replace("XBTUSDT", "BTCUSDT")
+                    order = active_orders[symbol]
+                    stop_price = data["stopPrice"]
+                    if data["stop"] == "up" and order["take_profit"] != stop_price:
+                        order["take_profit"] = stop_price
+                    else:
+                        order["stop_loss"] = stop_price
+                    message = text(
+                        (f"Futures Contract ⏳\n\nPosition Update ❗️❗️❗️\n\n"
+                         f"Coin: {bold(symbol)}\n"
+                         f"LONG/SHORT: {order['side']}\n"
+                         f"Entry: {order['entry']}\n"
+                         f"Leverage: 10-20x\n"
+                         f"Take Profit: {order['take_profit']}\n"
+                         f"Stop Loss: {order['stop_loss']}\n"))
+                    await send_message(channel_id=TELEGRAM_CHAT_ID, text=message)
+            elif "/contract/position" in msg["topic"]:
+                data = msg["data"]
+                symbol = msg["topic"].split(":")[1][:-1]
                 symbol = symbol.replace("XBTUSDT", "BTCUSDT")
-                entry = data["matchPrice"]
                 if symbol in active_orders:
-                    active_orders[symbol][
-                        "entry"] = f"{active_orders[symbol]['entry']}-{entry}"
-                else:
-                    active_orders[symbol] = {
-                        "entry": entry,
-                        "side": "SHORT" if data["side"] == "sell" else "LONG",
-                        "take_profit": "",
-                        "stop_loss": "",
-                        "pnl": "",
-                    }
-        elif msg["topic"] == "/contractMarket/advancedOrders":
-            data = msg["data"]
-            if data["type"] != "cancel":
-                symbol = data["symbol"][:-1]
-                symbol = symbol.replace("XBTUSDT", "BTCUSDT")
-                order = active_orders[symbol]
-                stop_price = data["stopPrice"]
-                if data["stop"] == "up" and order["take_profit"] != stop_price:
-                    order["take_profit"] = stop_price
-                else:
-                    order["stop_loss"] = stop_price
-                message = text(
-                    (f"Futures Contract ⏳\n\nPosition Update ❗️❗️❗️\n\n"
-                     f"Coin: {bold(symbol)}\n"
-                     f"LONG/SHORT: {order['side']}\n"
-                     f"Entry: {order['entry']}\n"
-                     f"Leverage: 10-20x\n"
-                     f"Take Profit: {order['take_profit']}\n"
-                     f"Stop Loss: {order['stop_loss']}\n"))
-                await send_message(channel_id=TELEGRAM_CHAT_ID, text=message)
-        elif "/contract/position" in msg["topic"]:
-            data = msg["data"]
-            symbol = msg["topic"].split(":")[1][:-1]
-            symbol = symbol.replace("XBTUSDT", "BTCUSDT")
-            if symbol in active_orders:
-                active_orders[symbol]["pnl"] = data["unrealisedPnl"]
+                    active_orders[symbol]["pnl"] = data["unrealisedPnl"]
+        except Exception as e:
+            logger.exception(e)
 
     # is private
     client = WsToken(
