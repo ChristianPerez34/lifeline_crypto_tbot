fec8d209c6d5c91e8b457a12ab7e43ddd11652e7
Restyled.io <commits@restyled.io>
2021-06-08 18:54:16 +0000

    Restyled by autopep8

--- handlers/crypto.py
+++ handlers/crypto.py
@@ -53,6 +53,7 @@ HEADERS = {
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
 }
 
+
 def get_coin_stats(symbol: str) -> dict:
     """Retrieves coinstats from connected services crypto services
 
