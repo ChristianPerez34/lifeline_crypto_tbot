9ea80e2d5463972c05d672c815bf4fb830a9ac19
Restyled.io <commits@restyled.io>
2021-06-08 16:06:27 +0000

    Restyled by black

--- api/bsc.py
+++ api/bsc.py
@@ -5,8 +5,7 @@ from web3 import Web3
 
 class BinanceSmartChain:
     def __init__(self):
-        self.web3 = Web3(Web3.HTTPProvider(
-            'https://bsc-dataseed.binance.org/'))
+        self.web3 = Web3(Web3.HTTPProvider("https://bsc-dataseed.binance.org/"))
 
     def get_account_balance(self, address: str) -> Decimal:
-        return self.web3.fromWei(self.web3.eth.get_balance(address), 'ether')
+        return self.web3.fromWei(self.web3.eth.get_balance(address), "ether")
