3c89af13fefa0defcbeb184483f4b326f4187578
Christian Perez Villanueva <perez.villanueva.christian34@gmail.com>
2021-06-09 12:24:05 -0400

    Updated logger

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pathlib import Path
 
 from etherscan import Etherscan
 from lru import LRU
@@ -8,10 +9,16 @@ from tortoise import Tortoise
 from config import DB_CONFIG
 
 # Enable logging
+log_file = str(Path.home().joinpath('logs/lifeline_crypto_tbot.log'))
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO)
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(log_file, mode='w+'),
+        logging.StreamHandler()
+    ]
+)
 
 logger = logging.getLogger(__name__)
 coingecko_coin_lookup_cache = LRU(5)
