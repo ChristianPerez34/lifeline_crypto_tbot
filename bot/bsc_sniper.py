838b448646b7826bd02d638806500affa71af424
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2021-07-19 09:39:12 -0400

    Added some logs to know if sniping is still working

--- bot/bsc_sniper.py
+++ bot/bsc_sniper.py
@@ -20,6 +20,7 @@ def token_has_liquidity(token, pancake_swap) -> bool:
     Returns: Boolean indicating token has liquidity
 
     """
+    logger.info("Verifying %s has liquidity")
     has_liquidity = False
     try:
         pair_address = pancake_swap.get_token_pair_address(token=token)
