b07e452a19982ef42d2d6d726020a47a77d96478
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2021-10-26 22:26:43 -0400

    Limit orders running in the background

--- app/routers/webhook.py
+++ app/routers/webhook.py
@@ -28,7 +28,7 @@ from handlers.crypto import (
     send_snipe,
     send_active_orders,
     send_cancel_order,
-    alert_inline_query_handler,
+    alert_inline_query_handler, send_limit_swap,
 )
 from handlers.error import send_error
 from handlers.user import send_register
@@ -111,7 +111,7 @@ def setup_handlers(dispatcher: Dispatcher) -> None:
     dispatcher.register_message_handler(send_sell, commands=["sell"])
     dispatcher.register_message_handler(send_spy, commands=["spy"])
     dispatcher.register_message_handler(send_snipe, commands=["snipe"])
-    # dispatcher.register_message_handler(send_limit_swap, commands=["limit"])
+    dispatcher.register_message_handler(send_limit_swap, commands=["limit"])
     dispatcher.register_message_handler(send_active_orders, commands=["active_orders"])
     dispatcher.register_message_handler(send_cancel_order, commands=["cancel_order"])
 
