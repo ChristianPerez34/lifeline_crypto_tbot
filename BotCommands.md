e2fd795e26a3dca67c42a5307cab43f39906340b
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2021-12-01 10:06:28 -0400

    Help command replies with PDF detailing all available commands

---
+++ BotCommands.md
@@ -0,0 +1,211 @@
+# Available Commands
+
+## /help
+
+Displays PDF document with available commands
+
+## /price _SYMBOL_
+
+Displays CoinGecko/CoinMarketCap statistics for the given cryptocurrency symbol.
+
+```shell
+/price btc
+```
+
+## /price_address _ADDRESS_ _PLATFORM_
+
+Displays price data for given contract address.
+
+Supported platforms
+
+* eth
+* bsc
+* matic
+
+```shell
+/price 0x0000000000000 eth
+```
+
+## /gas
+
+Displays ethereum gas fees
+
+## /trending
+
+Displays trending tokens on CoinGecko and CoinMarketCap
+
+## /alert _SYMBOL_ < OR > _TARGET_PRICE_
+
+Alerts user when specified token reaches overcomes target price
+
+```shell
+/alert btc > 60000
+```
+
+## /latest_listings
+
+Displays latest token listings on COinGecko and CoinMarketCap <br><br>
+
+## /register _NETWORK_ _PARAMETERS_
+
+Registers user wallet/exchange account to use with other commands for quick trade execution or viewing account balance.
+
+```shell
+/register bsc ADDRESS PRIVATE_KEY
+/register eth ADDRESS PRIVATE_KEY
+/register matic ADDRESS PRIVATE_KEY
+/register coinbase API_KEY API_SECRET API_PASSPHRASE
+/register kucoin API_KEY API_SECRET API_PASSPHRASE
+```
+
+## /buy _NETWORK_ _ADDRESS_ _AMOUNT_
+
+Purchases token from decentralized exchange
+
+Networks
+
+* bsc - AMOUNT in BNB
+* eth - AMOUNT in ETH
+* matic -AMOUNT in MATIC
+
+```shell
+/buy bsc 0x68fbd4d89ba15343e7d2457189459b7ac80a20a3 0.5
+```
+
+## /sell _NETWORK_ _ADDRESS_
+
+Sells all tokens for given address in a decentralized exchange
+
+Networks
+
+* bsc
+* eth
+* matic
+
+```shell
+/sell bsc 0x68fbd4d89ba15343e7d2457189459b7ac80a20a3
+```
+
+## /chart _SYMBOL_ _NUM_DAYS_
+
+Displays chart of given token symbol for a period of time.
+
+```shell
+/chart btc 365
+```
+
+<br><br>
+
+## /candle _SYMBOL-BASE_ _NUM_TIME_ _TIME_UNIT_
+
+Displays candle stick chart of given token symbol pair for a period of time.
+
+Time Units
+
+* d - Day
+* h - Hour
+* m - Minute
+
+```shell
+/candle btc-usd 365 d
+/candle btc-usd 7 h
+/candle btc-usd 30 m
+```
+
+## /balance _NETWORK_
+
+Replies privately with the balance of your current holdings for the given network
+
+Networks
+
+* bsc
+* eth
+* matic
+* coinbase
+
+```shell
+/balance eth
+```
+
+## /spy _NETWORK_ _ADDRESS_
+
+Displays 5 recent holdings for given address
+
+Networks
+
+* bsc
+* eth
+* matic
+
+```shell
+/spy eth 0x000000000000
+```
+
+<br><br><br><br><br><br>
+
+## /snipe _ADDRESS_ _BNB_AMOUNT_
+
+Utilizes high gas to purchase token as fast as possible. Will wait until liquidity is present before purchasing token
+
+```shell
+/snipe 0x68fbd4d89ba15343e7d2457189459b7ac80a20a3 0.5
+```
+
+## /limit _ACTION_ _ADDRESS_ _TARGET_PRICE_ _BNB_AMOUNT_
+
+Creates a limit order to trade on the binance smart chain
+
+**BNB_AMOUNT** only required for limit buy
+
+ACTIONS
+
+* buy
+* sell
+* stop
+
+```shell
+/limit sell 0x68fbd4d89ba15343e7d2457189459b7ac80a20a3 5
+```
+
+## /active_orders
+
+Displays active limit orders
+
+## /cancel_order _ORDER_ID_
+
+Cancels limit order
+
+```shell
+/cancel_order 30
+```
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
+
+## /coinbase _ORDER_TYPE_ _ACTION_ _SYMBOL_ _USD_AMOUNT_ _USD_LIMIT_PRICE_
+
+Creates a coinbase trade order
+
+Order types
+
+* limit
+* market
+
+Actions
+
+* buy
+* sell
+* stop - Only applies for limit orders
+
+**USD_LIMIT_PRICE only used on Limit orders**
+
+```shell
+/coinbase limit buy eth 100 3500
+/coinbase market buy eth 100
+```
+
+# Admin Commands
+
+## /restart_kucoin
+
+Restarts kucoin bot. This bot displays futures trades in real time (Currently disabled)
+
--- handlers/base.py
+++ handlers/base.py
@@ -1,8 +1,7 @@
 import random
 from io import BufferedReader
 
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ParseMode
-from aiogram.utils.markdown import bold, italic, text
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ParseMode, InputFile
 
 from app import bot
 from app import logger
@@ -17,52 +16,9 @@ async def send_welcome(message: Message) -> None:
     """
     logger.info("Chat ID: %s", message.chat.id)
     logger.info("Start/Help command executed")
-    reply = text(
-        "Hi! 😄\n",
-        "I'm Lifeline (Crypto)!\n\n",
-        f"{bold('/help')} to display available commands\n\n",
-        f"{bold('/price')} {italic('COIN')} to display coin statistics\n\n",
-        rf"{bold('/price')}\_{bold('address')} {italic('ADDRESS')} {italic('PLATFORM')} to display coin statistics for "
-        f"crypto address. {italic('PLATFORM')} is optional\n\n",
-        f"{bold('/gas')} to display ETH gas prices\n\n",
-        f"{bold('/trending')} to display trending coins\n\n",
-        f"{bold('/alert')} {italic('COIN')} " r"\[< or >] ",
-        f"{italic('PRICE')} to set an alert for when the coin reaches set price\n\n",
-        rf"{bold('/latest')}\_{bold('listings')} to display latest crypto listings"
-        "\n\n",
-        rf"{bold('/restart')}\_{bold('kucoin')} to restart KuCoin bot 🤖" "\n\n",
-        rf"{bold('/register')} {bold('bsc')} {italic('ADDRESS')} {italic('PRIVATE')}\_{italic('KEY')} to register to "
-        f"use PancakeSwap bot 🤖\n\n",
-        rf"{bold('/register')} {bold('kucoin')} {italic('API')}\_{italic('KEY')} {italic('API')}\_{italic('SECRET')} "
-        rf"{italic('API')}\_{italic('PASSPHRASE')} to register KuCoin account and follow signals"
-        "\n\n"
-        rf"{bold('/register')} {bold('coinbase')} {italic('API')}\_{italic('KEY')} {italic('API')}\_{italic('SECRET')} "
-        rf"{italic('API')}\_{italic('PASSPHRASE')} to register CoinBase account"
-        "\n\n"
-        rf"{bold('/buy')} {italic('NETWORK')} {italic('ADDRESS')} {italic('BNB')}\_{italic('AMOUNT')} to buy coins on "
-        "supported DEX's. Valid Networks are bsc|eth|matic\n\n",
-        f"{bold('/sell')} {italic('NETWORK')} {italic('ADDRESS')} to sell coins on supported DEX's. Valid Networks are "
-        "bsc|eth|matic\n\n",
-        rf"{bold('/chart')} {italic('COIN')}-{italic('BASECOIN')} {italic('NUM')}\_{italic('DAYS')} to display coin "
-        "chart. If BaseCoin not specified, will default to USD\n\n",
-        rf"{bold('/candle')} {italic('COIN')}-{italic('BASECOIN')} {italic('NUM')}\_{italic('TIME')} {italic('LETTER')}"
-        " to display coin candle chart. If BaseCoin not specified, will default to USD\n\n",
-        f"{bold('/balance')} {italic('NETWORK')} to display binance smart chain balance. Responds privately."
-        " Valid networks eth|bsc|matic|coinbase\n\n",
-        f"{bold('/spy')} {italic('ADDRESS')} to display some of the accounts holdings.\n\n",
-        rf"{bold('/snipe')} {italic('ADDRESS')} {italic('BNB')}\_{italic('AMOUNT')} to snipe token."
-        " Uses high gas!\n\n",
-        rf"{bold('/limit')} {italic('ACTION')} {italic('ADDRESS')} {italic('TARGET')}\_{italic('PRICE')} "
-        rf"{italic('BNB')}\_{italic('AMOUNT')} to create a limit order. "
-        "Actions: buy|sell|stop\n\n",
-        rf"{bold('/active')}\_{bold('orders')} to display active limit orders" "\n\n",
-        rf"{bold('/cancel')}\_{bold('order')} {italic('ORDER')}\_{italic('ID')} to cancel order"
-        "\n\n",
-        f"{bold('/coinbase')} {bold('limit')} {italic('BUY')}|{italic('SELL')}|{italic('STOP')} {italic('SYMBOL')} "
-        f"{italic('$AMOUNT')} {italic('$LIMIT')}\n\n",
-        f"{bold('/coinbase')} {bold('market')} {italic('BUY')}|{italic('SELL')} {italic('SYMBOL')}\n\n",
-    )
-    await message.reply(text=reply, parse_mode=ParseMode.MARKDOWN)
+    pdf = InputFile("BotCommands.pdf")
+    await message.reply_document(document=pdf, caption="PDF detailing available bot commands",
+                                 parse_mode=ParseMode.MARKDOWN)
 
 
 async def send_greeting(message: Message) -> None:
@@ -84,7 +40,7 @@ async def send_greeting(message: Message) -> None:
 
 
 async def send_message(
-    channel_id: int, message: str, inline: bool = False, data: str = ""
+        channel_id: int, message: str, inline: bool = False, data: str = ""
 ) -> None:
     logger.info("Sending message to chat id: %s", channel_id)
     keyboard_markup = InlineKeyboardMarkup()
