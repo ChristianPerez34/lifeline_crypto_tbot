8f22670bebccd16214d6dc9ef2015884790ff09a
autoblack <cclauss@users.noreply.github.com>
2022-07-15 05:09:03 +0000

    fixup: Format Python code with Black

--- models.py
+++ models.py
@@ -30,11 +30,11 @@ class TelegramGroupMember(db.Entity):  # type: ignore
                         for member in TelegramGroupMember  # type: ignore
                         if member.id == primary_key
                     )
-                        .prefetch(BinanceNetwork)
-                        .prefetch(EthereumNetwork)
-                        .prefetch(MaticNetwork)
-                        .prefetch(CoinBase)
-                        .first()
+                    .prefetch(BinanceNetwork)
+                    .prefetch(EthereumNetwork)
+                    .prefetch(MaticNetwork)
+                    .prefetch(CoinBase)
+                    .first()
                 )
             except orm.ObjectNotFound:
                 return None
@@ -191,11 +191,11 @@ class Order(db.Entity):  # type: ignore
             try:
                 return (
                     orm.select(order for order in Order if order.id == primary_key)  # type: ignore
-                        .prefetch(TelegramGroupMember)
-                        .prefetch(TelegramGroupMember.bsc)
-                        .prefetch(TelegramGroupMember.eth)
-                        .prefetch(TelegramGroupMember.matic)
-                        .first()
+                    .prefetch(TelegramGroupMember)
+                    .prefetch(TelegramGroupMember.bsc)
+                    .prefetch(TelegramGroupMember.eth)
+                    .prefetch(TelegramGroupMember.matic)
+                    .first()
                 )
             except orm.ObjectNotFound:
                 return None
@@ -213,10 +213,10 @@ class Order(db.Entity):  # type: ignore
                         for order in Order  # type: ignore
                         if order.telegram_group_member.id == telegram_group_member_id
                     )
-                        .prefetch(TelegramGroupMember)
-                        .prefetch(TelegramGroupMember.bsc)
-                        .prefetch(TelegramGroupMember.eth)
-                        .prefetch(TelegramGroupMember.matic)
+                    .prefetch(TelegramGroupMember)
+                    .prefetch(TelegramGroupMember.bsc)
+                    .prefetch(TelegramGroupMember.eth)
+                    .prefetch(TelegramGroupMember.matic)
                 )
             )
 
@@ -232,10 +232,10 @@ class Order(db.Entity):  # type: ignore
         with orm.db_session:
             return list(
                 Order.select()
-                    .prefetch(TelegramGroupMember)
-                    .prefetch(TelegramGroupMember.bsc)
-                    .prefetch(TelegramGroupMember.eth)
-                    .prefetch(TelegramGroupMember.matic)
+                .prefetch(TelegramGroupMember)
+                .prefetch(TelegramGroupMember.bsc)
+                .prefetch(TelegramGroupMember.eth)
+                .prefetch(TelegramGroupMember.matic)
             )
 
     @orm.db_session
@@ -274,9 +274,7 @@ class MonthlySubmission(db.Entity):  # type: ignore
     @staticmethod
     def all() -> list:
         with orm.db_session:
-            return list(
-                MonthlySubmission.select()
-            )
+            return list(MonthlySubmission.select())
 
     @staticmethod
     def create(data: dict) -> db.Entity:  # type: ignore
